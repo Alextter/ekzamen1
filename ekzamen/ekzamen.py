bf18a9e3a32b519c1b3e76e1c21fe72b60777368
Alextter <74066029+Alextter@users.noreply.github.com>
2021-11-27 16:04:28 +0200

    Add files via upload

---
+++ ekzamen/ekzamen.py
@@ -0,0 +1,89 @@
+#1
+#a =[1,1,2,3,5,8,13,21,34,55,89]
+#for i in a:
+    #if i<5:
+        #print(i)
+
+#2
+#import random
+#a=1
+#n=int(input("="))
+#spisok=[random.randint(0,15) for i in range(n)]
+#for i in spisok:
+#    a*=i
+#print(a)
+#print(spisok)
+    
+    
+
+#3
+#numbers = [386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217] 
+#for i in numbers:
+#    if i%2==0:
+#        print(i)
+#    elif i == 237:
+#        break
+    
+
+#4
+#age=int(input("Введите возраст"))
+#if age<2:
+#    print("младенец")
+#elif age>=4 and age<13:
+#    print("ребенок")
+#elif age>=13 and age<20:
+#    print("подросток")
+#elif age>=20 and age<65:
+#    print("взрослый")
+#elif age>=65:
+#    print("старый")
+
+#5
+#a =["Misha","Masha","Denis"]
+#b=[]
+#while a:
+#    del_user=a.pop(0)
+#    b.append(del_user)
+#for i in b:
+#    print(f"user {i} is ferificated")
+    
+#6
+bl=['pizza', 'falafel', 'carrot cake']
+dl=bl[:]
+print(bl)
+print(dl)
+m=(input("bl="))
+dl.append(m)
+print(dl)
+print("=======")
+for i in dl:
+    print(i)
+print("=======")
+for i in bl:
+    print(i)   
+
+#7
+#a=0
+#st = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
+#for i in st:
+#    if i<30 and i%3==0:
+#        print(i)
+#for i in st:
+#    i+=1
+#print(a)
+
+#8
+#slovar={'a': 2, 'b': 4, 'c': 6, 'd': 8}
+#for x,y in slovar.items():
+#    if y>2:
+#        print(y)
+        
+
+    
+
+
+
+
+
+
+    
